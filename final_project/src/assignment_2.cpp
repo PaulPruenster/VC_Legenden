db6b6d17e427150dc8eaffb608339542c3962157
PaulPruenster <paul.pruenster@gmail.com>
2024-01-15 16:38:29 +0100

    cleanup (still broken)

--- final_project/src/assignment_2.cpp
+++ final_project/src/assignment_2.cpp
@@ -15,13 +15,10 @@ struct
     bool isDay = true;
     float zoomSpeedMultiplier;
 
-    Boat boat;
-    Model water;
+    Model city;
 
-    ShaderProgram shaderBoat;
-    ShaderProgram shaderWater;
+    ShaderProgram shaderCity;
 
-    WaterSim waterSim;
 } sScene;
 
 struct
@@ -127,21 +124,9 @@ void sceneInit(float width, float height)
     sScene.cameraFollowBoat = true;
     sScene.zoomSpeedMultiplier = 0.05f;
 
-    sScene.boat = boatLoad("assets/Lowpoly_City_Free_Pack.obj");
-    sScene.water = modelLoad("assets/water/water.obj").front();
+    sScene.city = modelLoad("assets/Lowpoly_City_Free_Pack.obj").front();
 
-    sScene.shaderBoat = shaderLoad("shader/default.vert", "shader/color.frag");
-    sScene.shaderWater = shaderLoad("shader/water.vert", "shader/color.frag");
-}
-
-void sceneUpdate(float dt)
-{
-    sScene.waterSim.accumTime += dt;
-
-    boatMove(sScene.boat, sScene.waterSim, sInput.keyPressed, dt);
-
-    if (!sScene.cameraFollowBoat)
-        cameraFollow(sScene.camera, sScene.boat.position);
+    sScene.shaderCity = shaderLoad("shader/default.vert", "shader/color.frag");
 }
 
 Vector3D getLightColor()
@@ -153,126 +138,34 @@ Vector3D getLightColor()
     return Vector3D(31 / 255.0f, 31 / 255.0f, 31 / 255.0f);
 }
 
-Vector3D getLightPosition(Vector3D lighOffset)
-{
-    Vector4D offsetHomogeneous = Vector4D(lighOffset, 1);
-    Vector4D offsetWorld = sScene.boat.transformation * offsetHomogeneous;
-    Vector3D offset = Vector3D(offsetWorld);
-    return offset;
-}
-
 void render()
 {
     /* setup camera and model matrices */
     Matrix4D proj = cameraProjection(sScene.camera);
     Matrix4D view = cameraView(sScene.camera);
-    glUseProgram(sScene.shaderBoat.id);
-    shaderUniform(sScene.shaderBoat, "uProj", proj);
-    shaderUniform(sScene.shaderBoat, "uView", view);
-    shaderUniform(sScene.shaderBoat, "uModel", sScene.boat.transformation);
 
-    shaderUniform(sScene.shaderBoat, "uDirectionalLight.direction", normalize(Vector3D(0.0f, -0.2f, 1.0f)));
-    shaderUniform(sScene.shaderBoat, "cameraPosition", sScene.camera.position);
-    shaderUniform(sScene.shaderBoat, "uDirectionalLight.color", getLightColor());
+    shaderUniform(sScene.shaderCity, "uDirectionalLight.direction", normalize(Vector3D(0.0f, -0.2f, 1.0f)));
+    shaderUniform(sScene.shaderCity, "cameraPosition", sScene.camera.position);
+    shaderUniform(sScene.shaderCity, "uDirectionalLight.color", getLightColor());
 
-    Vector3D lightPosition_green = getLightPosition(Vector3D(0.5f, 2.0f, -1.5f));
-    Vector3D lightPosition_red = getLightPosition(Vector3D(-0.5f, 2.0f, -1.5f));
-    Vector3D lightPosition_white = getLightPosition(Vector3D(-0.5f, 2.0f, -0.1f));
-    Vector3D lightPosition_white2 = getLightPosition(Vector3D(0.5f, 2.0f, -0.1f));
-
-    shaderUniform(sScene.shaderBoat, "uSpotLights[0].position", lightPosition_green);
-    shaderUniform(sScene.shaderBoat, "uSpotLights[0].color", Vector3D(0.0f, 1.0f, 0.0f));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[0].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(1.0f, 0.0f, 0.0f)));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[0].angle", 1.57f);
-
-    shaderUniform(sScene.shaderBoat, "uSpotLights[1].position", lightPosition_red);
-    shaderUniform(sScene.shaderBoat, "uSpotLights[1].color", Vector3D(1.0f, 0.0f, 0.0f));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[1].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(-1.0f, 0.0f, 0.0f)));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[1].angle", 1.57f);
-
-    shaderUniform(sScene.shaderBoat, "uSpotLights[2].position", lightPosition_white);
-    shaderUniform(sScene.shaderBoat, "uSpotLights[2].color", Vector3D(1.0f, 1.0f, 1.0f));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[2].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(0.0f, -0.2f, -1.0f)));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[2].angle", 1.309f);
-
-    shaderUniform(sScene.shaderBoat, "uSpotLights[3].position", lightPosition_white2);
-    shaderUniform(sScene.shaderBoat, "uSpotLights[3].color", Vector3D(1.0f, 1.0f, 1.0f));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[3].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * (0.0f, -0.0f, -1.0f)));
-    shaderUniform(sScene.shaderBoat, "uSpotLights[3].angle", 1.309f);
-
-    for (unsigned int i = 0; i < sScene.boat.partModel.size(); i++)
+    for (auto &material : sScene.city.material)
     {
-        auto &model = sScene.boat.partModel[i];
-        glBindVertexArray(model.mesh.vao);
-
-        shaderUniform(sScene.shaderBoat, "uModel", sScene.boat.transformation);
-
-        for (auto &material : model.material)
-        {
-            /* set material properties */
-            shaderUniform(sScene.shaderBoat, "uMaterial.diffuse", material.diffuse);
-            shaderUniform(sScene.shaderBoat, "uMaterial.shininess", material.shininess);
-            shaderUniform(sScene.shaderBoat, "uMaterial.ambient", material.ambient);
-            shaderUniform(sScene.shaderBoat, "uMaterial.specular", material.specular);
-
-            glDrawElements(GL_TRIANGLES, material.indexCount, GL_UNSIGNED_INT, (const void *)(material.indexOffset * sizeof(unsigned int)));
-        }
-    }
-
-    /* render water */
-    {
-        glUseProgram(sScene.shaderWater.id);
-
-        /* setup camera and model matrices */
-        shaderUniform(sScene.shaderWater, "uProj", proj);
-        shaderUniform(sScene.shaderWater, "uView", view);
-        shaderUniform(sScene.shaderWater, "uModel", Matrix4D::identity());
-
-        shaderUniform(sScene.shaderWater, "uDirectionalLight.direction", Vector3D(0.0f, -1.0f, 0.0f));
-        shaderUniform(sScene.shaderWater, "cameraPosition", sScene.camera.position);
-        shaderUniform(sScene.shaderWater, "uDirectionalLight.color", getLightColor());
-
-        shaderUniform(sScene.shaderWater, "uSpotLights[0].position", lightPosition_green);
-        shaderUniform(sScene.shaderWater, "uSpotLights[0].color", Vector3D(0.0f, 1.0f, 0.0f));
-        shaderUniform(sScene.shaderWater, "uSpotLights[0].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(1.0f, 0.0f, 0.0f)));
-        shaderUniform(sScene.shaderWater, "uSpotLights[0].angle", 1.57f);
-
-        shaderUniform(sScene.shaderWater, "uSpotLights[1].position", lightPosition_red);
-        shaderUniform(sScene.shaderWater, "uSpotLights[1].color", Vector3D(1.0f, 0.0f, 0.0f));
-        shaderUniform(sScene.shaderWater, "uSpotLights[1].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(-1.0f, 0.0f, 0.0f)));
-        shaderUniform(sScene.shaderWater, "uSpotLights[1].angle", 1.57f);
-
-        shaderUniform(sScene.shaderWater, "uSpotLights[2].position", lightPosition_white);
-        shaderUniform(sScene.shaderWater, "uSpotLights[2].color", Vector3D(1.0f, 1.0f, 1.0f));
-        shaderUniform(sScene.shaderWater, "uSpotLights[2].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(0.0f, 0.3f, -1.0f)));
-        shaderUniform(sScene.shaderWater, "uSpotLights[2].angle", 0.655f);
-
-        shaderUniform(sScene.shaderWater, "uSpotLights[3].position", lightPosition_white2);
-        shaderUniform(sScene.shaderWater, "uSpotLights[3].color", Vector3D(1.0f, 1.0f, 1.0f));
-        shaderUniform(sScene.shaderWater, "uSpotLights[3].direction", normalize(Matrix3D::rotationY(sScene.boat.angles.y) * Vector3D(0.0f, 0.3f, -1.0f)));
-        shaderUniform(sScene.shaderWater, "uSpotLights[3].angle", 0.655f);
-
-        shaderUniform(sScene.shaderWater, "uWaterSim.accumTime", sScene.waterSim.accumTime);
-        for (int i = 0; i < 3; i++)
-        {
-            WaveParams current = sScene.waterSim.parameter[i];
-            std::string currentStr = "uWaterSim.parameter[" + std::to_string(i) + "]";
-
-            shaderUniform(sScene.shaderWater, currentStr + ".amplitude", current.amplitude);
-            shaderUniform(sScene.shaderWater, currentStr + ".phi", current.phi);
-            shaderUniform(sScene.shaderWater, currentStr + ".omega", current.omega);
-            shaderUniform(sScene.shaderWater, currentStr + ".direction", current.direction);
-        }
-
         /* set material properties */
-        shaderUniform(sScene.shaderWater, "uMaterial.diffuse", sScene.water.material.front().diffuse);
-        shaderUniform(sScene.shaderWater, "uMaterial.shininess", sScene.water.material.front().shininess);
-        shaderUniform(sScene.shaderWater, "uMaterial.ambient", sScene.water.material.front().ambient);
-        shaderUniform(sScene.shaderWater, "uMaterial.specular", sScene.water.material.front().specular);
+        shaderUniform(sScene.shaderCity, "uMaterial.diffuse", material.diffuse);
+        shaderUniform(sScene.shaderCity, "uMaterial.shininess", material.shininess);
+        shaderUniform(sScene.shaderCity, "uMaterial.ambient", material.ambient);
+        shaderUniform(sScene.shaderCity, "uMaterial.specular", material.specular);
 
-        glBindVertexArray(sScene.water.mesh.vao);
-        glDrawElements(GL_TRIANGLES, sScene.water.material.front().indexCount, GL_UNSIGNED_INT, (const void *)(sScene.water.material.front().indexOffset * sizeof(unsigned int)));
+        glDrawElements(GL_TRIANGLES, material.indexCount, GL_UNSIGNED_INT, (const void *)(material.indexOffset * sizeof(unsigned int)));
     }
+
+    /* setup camera and model matrices */
+    glUseProgram(sScene.shaderCity.id);
+    shaderUniform(sScene.shaderCity, "uProj", proj);
+    shaderUniform(sScene.shaderCity, "uView", view);
+    shaderUniform(sScene.shaderCity, "uModel", Matrix4D::identity());
+
+    glBindVertexArray(sScene.city.mesh.vao);
 
     /* cleanup opengl state */
     glBindVertexArray(0);
@@ -330,7 +223,6 @@ int main(int argc, char **argv)
 
         /* update scene */
         timeStampNew = glfwGetTime();
-        sceneUpdate(timeStampNew - timeStamp);
         timeStamp = timeStampNew;
 
         /* draw all objects in the scene */
@@ -341,10 +233,8 @@ int main(int argc, char **argv)
     }
 
     /*-------- cleanup --------*/
-    boatDelete(sScene.boat);
-    modelDelete(sScene.water);
-    shaderDelete(sScene.shaderBoat);
-    shaderDelete(sScene.shaderWater);
+    modelDelete(sScene.city);
+    shaderDelete(sScene.shaderCity);
     windowDelete(window);
 
     return EXIT_SUCCESS;
